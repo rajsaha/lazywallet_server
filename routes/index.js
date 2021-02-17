48d2106d4a9b4004e39a9c87ebc3758db3865419
Raj Saha <rajsaha@outlook.com>
2021-02-17 17:04:24 +0800

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+import express from 'express';
+let indexRouter = express.Router();
+
+/* GET home page. */
+indexRouter.get('/', (req, res, next) => {
+  res.send('Graphql');
+});
+
+module.exports = indexRouter;
